d789f2f3d90bfbeaa57642747d2d0cb9ef266c0e
Pixelastic <tim@pixelastic.com>
2016-08-26 01:15:25 +0200

    Adding deploy script

--- app/app.js
+++ app/app.js
@@ -32,7 +32,7 @@ let Search = {
     this.search.start();
   },
   cloudinary(url, options) {
-    let baseUrl = 'http://res.cloudinary.com/pixelastic-parisweb/image/fetch/';
+    let baseUrl = 'https://res.cloudinary.com/pixelastic-parisweb/image/fetch/';
     let stringOptions = [];
 
     // http://res.cloudinary.com/pixelastic-parisweb/image/fetch/h_50,q_90,c_scale,r_max,f_auto/https://www.paris-web.fr/2015/assets_c/2015/05/Martin%20Naumann-thumb-143x143-487.jpg
@@ -82,8 +82,8 @@ let Search = {
     }
     Search.lazyloadCounter++;
 
-    let isConference = data.type == 'conference';
-    let isWorkshop = data.type == 'workshop';
+    let isConference = data.type == 'Conférence';
+    let isWorkshop = data.type == 'Atelier';
 
     let description = data._snippetResult.description.value;
     description = description.replace(' …', '…');
